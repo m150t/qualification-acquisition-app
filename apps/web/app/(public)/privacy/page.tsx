9989b9259e5cbec2477c7d0cec70f7a9bce1983f
m150t <77564012+m150t@users.noreply.github.com>
2025-12-26 18:02:35 +0900

    Update page.tsx

--- apps/web/app/(public)/privacy/page.tsx
+++ apps/web/app/(public)/privacy/page.tsx
@@ -1,7 +1,8 @@
-import Privacy from import Privacy from "../../../src/components/Privacy";
+import Privacy from "../../../src/components/Privacy";
 
 export default function Page() {
   return <Privacy />;
 }
+
 
 
