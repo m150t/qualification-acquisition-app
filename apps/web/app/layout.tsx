41208e2c4264144659930a9823f1d3f98e5d9715
m150t <mt4770@gmail.com>
2026-01-01 01:57:20 +0900

    広告追加

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import Providers from './Providers';
+import Script from "next/script";
+import Providers from "./Providers";
 
 export const metadata: Metadata = {
   title: "QUALog",
@@ -11,6 +12,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ja">
       <body>
+        {/* AdSense: 審査通すまで一旦グローバルに入れる */}
+        <Script
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3961829109868432"
+          crossOrigin="anonymous"
+          strategy="beforeInteractive"
+        />
+
         <Providers>{children}</Providers>
       </body>
     </html>
