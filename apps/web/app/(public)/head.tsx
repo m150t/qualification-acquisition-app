c959e0ad694b74217bda77d75d7f6c4d50efed24
m150t <77564012+m150t@users.noreply.github.com>
2026-01-16 02:39:48 +0900

    Integrate Google Tag Manager in head.tsx
    Added Google Tag Manager script for tracking.

--- apps/web/app/(public)/head.tsx
+++ apps/web/app/(public)/head.tsx
@@ -6,6 +6,15 @@ export default function Head() {
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3961829109868432"
         crossOrigin="anonymous"
       />
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-REKGLY7BW6"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-REKGLY7BW6');
+      </script>
     </>
   );
 }
