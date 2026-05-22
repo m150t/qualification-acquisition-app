687ab5722a6a783e1e932d516acd49f60ab60967
m150t <mt4770@gmail.com>
2026-05-23 01:21:36 +0900

    fix

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -13,6 +13,15 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/ban-ts-comment": [
+        "error",
+        { "ts-expect-error": "allow-with-description" },
+      ],
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+  },
 ]);
 
 export default eslintConfig;
